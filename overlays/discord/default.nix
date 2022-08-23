c5f4eb859e1cfa587b05d2ae188f15831d74fc02
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-08-23 15:57:17 +0200

    feat(discord): Bump discord version
    Bump discord version to 0.0.19.

--- overlays/discord/default.nix
+++ overlays/discord/default.nix
@@ -1,12 +1,12 @@
 let
-  version = "0.0.18";
+  version = "0.0.19";
 in
 self: super: {
   discord = super.discord.overrideAttrs (
     _: {
       src = builtins.fetchurl {
         url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-        sha256 = "1hl01rf3l6kblx5v7rwnwms30iz8zw6dwlkjsx2f1iipljgkh5q4";
+        sha256 = "1403vdc7p6a8mhr114brfp4dqvikaj5s71wrx20ca5y6srsv5x0r";
       };
     }
   );
