ec5df5bb0b9c6808cc690bb4070c25eaaf16250e
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2024-08-28 20:10:51 +0200

    lambda: Rename 32bit opengl option

--- hosts/lambda/default.nix
+++ hosts/lambda/default.nix
@@ -37,7 +37,7 @@
       extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1"; # Needed by mpd to be able to use Pulseaudio
     };
 
-    opengl.driSupport32Bit = true;
+    graphics.enable32Bit = true;
   };
 
   time.timeZone = "Europe/Paris";
